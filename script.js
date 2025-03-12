30c38d7a5300fde819459fbf370f587b45ebc4cb
Klauss1004 <suhas.mshivanand@gmail.com>
2025-03-13 00:35:14 +0530

    game completed

--- script.js
+++ script.js
@@ -2,22 +2,68 @@ console.log("Rock paper scissor")
 let rock=document.querySelector('#box1');
 let paper=document.querySelector('#box2');
 let sxr=document.querySelector('#box3');
+let play=document.querySelector("#play");
+let msg= document.querySelector('.msg');
+let userCt=document.querySelector('#userCt')
+let compCt=document.querySelector('#compCt')
 let Selction=['rock', 'paper','sxr']
 let user
-let comp
-
+let comp='sxr'
+let userC=0;
+let compC=0;
+let userWin=true
 rock.addEventListener('click',()=>{
     user='rock'
-    console.log('rock selected')
-    comp = randomSelection();
-    console.log(comp)
+   play.innerText = "Play";
+   play.style.backgroundColor = 'black'
 })
 paper.addEventListener('click',()=>{
-    console.log('paper selected')
+    user='paper'
+    play.innerText = "Play"
+    play.style.backgroundColor = 'black'
 })
 sxr.addEventListener('click',()=>{
-    console.log('scrs selected')
+    user='sxr'
+    play.innerText = "Play"
+    play.style.backgroundColor = 'black'
 })
+play.addEventListener('click',()=>{
+   
+    console.log("User choice =" , user);
+
+   randomSelection();
+    console.log(" comp choice =",comp)
+    if(user === comp){
+        play.innerText = "The Game is drawn. Play Again"
+    }
+    else{
+        if(user === 'rock'){
+            userWin = comp == 'sxr'? true:false
+        }
+        else if(user === 'paper'){
+            userWin = comp == 'rock'? true:false
+        }
+        else{
+            userWin = comp == 'paper'? true:false
+        }
+        showWinner();
+    }
+   
+})
+function showWinner(){
+    if(userWin){
+        userC++;
+        userCt.innerText = userC
+        play.innerText = "You Win"
+        play.style.backgroundColor = 'Green'
+    }
+    else{
+        compC++;
+        compCt.innerText= compC
+        play.innerText = "You Lose"
+         play.style.backgroundColor = 'red'
+    }
+}
 function randomSelection(){
-      return Selction.random()
+    comp=Selction[Math.floor([Math.random()*3])] // math.random*3 is used to crate radnom index upto 3
 }
